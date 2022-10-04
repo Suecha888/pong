5a714cc23c7fff6b8a3a26a7d7196364a1af5ea4
mio-makiya <95130352+mio-makiya@users.noreply.github.com>
2022-10-04 12:24:01 +0900

    ボールの処理の追加、physicMaterialの削除

--- PONG/Assets/Makiya/Script/Ball.cs
+++ PONG/Assets/Makiya/Script/Ball.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float speed = 3.0f;
+    private Rigidbody rb;
+    // ボールが当たった物体の法線ベクトル
+    private Vector3 objNomalVector = Vector3.zero;
+    // 跳ね返った後のverocity
+    private Vector3 afterReflectVero = Vector3.zero;
+    public GameObject ball;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(speed, speed, 0);
+            // 発射時のvelocityを取得
+            afterReflectVero = rb.velocity;
+        }
+
+        // 画面外にボールが出た時
+        if(transform.position.x >= 11 || transform.position.x <= -11)
+        {
+            Instantiate(ball, Vector3.zero, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Wall")
+        {
+            // 当たった物体の法線ベクトルを取得
+            objNomalVector = collision.contacts[0].normal;
+            Vector3 reflectVec = Vector3.Reflect(afterReflectVero, objNomalVector);
+            rb.velocity = reflectVec;
+
+            // 計算した反射ベクトルを保存
+            afterReflectVero = rb.velocity;
+        }
+
     }
 }
