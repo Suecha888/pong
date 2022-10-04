a212edf6749a23198b3ad0b81a19f25bf084c889
mio-makiya <95130352+mio-makiya@users.noreply.github.com>
2022-10-04 16:49:14 +0900

    no message

---
+++ PONG/Assets/Makiya/Script/AvatarNameDisplay.cs
@@ -0,0 +1,20 @@
+using Photon.Pun;
+using TMPro;
+
+// MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
+public class AvatarNameDisplay : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var nameLabel = GetComponent<TextMeshPro>();
+        // プレイヤー名とプレイヤーIDを表示する
+        nameLabel.text = $"{photonView.Owner.NickName}({photonView.OwnerActorNr})";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
