6a136348ee9c65add85a2b3c397d4a88a83e3e25
mio-makiya <95130352+mio-makiya@users.noreply.github.com>
2022-10-07 11:31:52 +0900

    Avatarのプレハブと処理の追加

--- PONG/Assets/Makiya/Script/AvatarController.cs
+++ PONG/Assets/Makiya/Script/AvatarController.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 // MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
 public class AvatarController : MonoBehaviourPunCallbacks
 {
+    public float speed = 3.0f;
+
     // Update is called once per frame
     private void Update()
     {
         // 自身が生成したオブジェクトだけに移動処理を行う
         if(photonView.IsMine)
         {
-            //var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-            //transform.Translate(6f * Time.deltaTime * input.normalized);
+            var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+            transform.Translate(speed * Time.deltaTime * input.normalized);
+
+            // 上移動
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    transform.position += transform.up * speed * Time.deltaTime;
+            //}
+            // 下移動
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    transform.position -= transform.up * speed * Time.deltaTime;
+            //}
         }
     }
 }
