0d952ccf4ee4b840199bac6e18e138c12febfa20
suecha888 <game.create.programmer@gmail.com>
2022-10-17 11:56:08 +0900

    ゲームシーンにあるオブジェクト（生成されるオブジェクトを除く）にアタッチされたスクリプトのコード整理

--- PONG/Assets/Suezaki/Script/CreateLine.cs
+++ PONG/Assets/Suezaki/Script/CreateLine.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class CreateLine : MonoBehaviour
 {
+    // ラインを構成する１ピースのオブジェクト
     [SerializeField] GameObject line_peace;
+    // セットの数
     [SerializeField,Min(1)] int set_num;
+    // ピースとピースの間隔
     [SerializeField,Min(0)] float space;
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < set_num; i++)
         {
+            // ピース（上）
             GameObject peace_up = Instantiate(line_peace);
-            GameObject peace_down = Instantiate(line_peace);
             peace_up.transform.position = new Vector3(0, space * i, 0);
-            peace_down.transform.position = new Vector3(0, -space * i, 0);
             peace_up.transform.parent = transform;
+            // ピース（下）
+            GameObject peace_down = Instantiate(line_peace);
+            peace_down.transform.position = new Vector3(0, -space * i, 0);
             peace_down.transform.parent = transform;
         }
     }
@@ -32,18 +37,16 @@ public class CreateLine : MonoBehaviour
         // 更新
         for (int i = 0; i < set_num; i++)
         {
+            // ピース（上）
             GameObject peace_up = Instantiate(line_peace);
-            GameObject peace_down = Instantiate(line_peace);
             peace_up.transform.position = new Vector3(0, space * i, 0);
-            peace_down.transform.position = new Vector3(0, -space * i, 0);
             peace_up.transform.parent = transform;
+            // ピース（下）
+            GameObject peace_down = Instantiate(line_peace);
+            peace_down.transform.position = new Vector3(0, -space * i, 0);
             peace_down.transform.parent = transform;
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
--- PONG/Assets/Suezaki/Script/CreateNetworkObj.cs
+++ PONG/Assets/Suezaki/Script/CreateNetworkObj.cs
@@ -10,20 +10,18 @@ public class CreateNetworkObj : MonoBehaviour
     {
         if (PhotonNetwork.IsMasterClient)
         {
+            // ルームオブジェクトの生成
             PhotonNetwork.InstantiateRoomObject("ball", new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
             PhotonNetwork.InstantiateRoomObject("pressbutton", new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
             PhotonNetwork.InstantiateRoomObject("score", new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
             PhotonNetwork.InstantiateRoomObject("gamescene", new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
-
+            // プレイヤーの生成
             PhotonNetwork.Instantiate("Player1", new Vector3(-8.5f, 0.0f, 0.0f), Quaternion.identity);
         }
         else
+            // プレイヤーの生成
             PhotonNetwork.Instantiate("Player1", new Vector3(8.5f, 0.0f, 0.0f), Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
