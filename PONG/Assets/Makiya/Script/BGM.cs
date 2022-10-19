0dd11b8dd3d018ebeb2b291cbf1d01b23c9cf101
mio-makiya <95130352+mio-makiya@users.noreply.github.com>
2022-10-19 12:42:29 +0900

    BGMとSEの追加

---
+++ PONG/Assets/Makiya/Script/BGM.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 画面遷移してもオブジェクトが壊れないようにする
+        DontDestroyOnLoad(this);
+    }
+}
--- PONG/Assets/Makiya/Script/testButton.cs
+++ PONG/Assets/Makiya/Script/testButton.cs
@@ -9,22 +9,32 @@ public class testButton : MonoBehaviourPunCallbacks
     public GameObject TitlePanel;
     public GameObject InputNamePanel;
     public GameObject RoomPanel;
+    public AudioClip SE1;
+    AudioSource audioSource;
     private RoomList roomList = new RoomList();     // 作成したルームのリスト
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void ClickStart()
     {
         // Photonサーバーに接続していなかったら
         if (!PhotonNetwork.IsConnected)
         {
-            TitlePanel.SetActive(false);
+            // 音を鳴らす
+            audioSource.PlayOneShot(SE1);
             InputNamePanel.SetActive(true);
+            TitlePanel.SetActive(false);
         }
         else
         {
             roomList.Clear();
-            TitlePanel.SetActive(false);
+            // 音を鳴らす
+            audioSource.PlayOneShot(SE1);
             RoomPanel.SetActive(true);
+            TitlePanel.SetActive(false);
         }
     }
-
 }
